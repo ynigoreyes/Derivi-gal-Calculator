d34b14d1236b70fea00390ad72369be46592455b
ynigoreyes <26727346+ynigoreyes@users.noreply.github.com>
2018-03-19 02:57:44 -0500

    heroku commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, request, session, url_for
+import json
+import sympy
+
+app = Flask(__name__)
+app.secret_key = 'DGCalc'
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run()
--- src/app.py
+++
@@ -1,2 +0,0 @@
-if __name__ == "__main__":
-    print("Ready to do some math?")
