7814f8e96648dfa82b0ab2dc4390e3f7919f0cdc
ynigoreyes <26727346+ynigoreyes@users.noreply.github.com>
2018-03-20 12:08:28 -0500

    removed dataType from ajax post and replace with contentType

--- src/static/script.js
+++ src/static/script.js
@@ -15,8 +15,7 @@ expr.value = 'sin(x)';
 pretty.innerHTML = '$$\\int(' + math.parse(expr.value).toTex({parenthesis: parenthesis}) + ') dx$$';
 result.innerHTML = '<span style="color: green;"> Press Solve to Evaluate!</span>';
 
-// result.innerHTML = math.format(math.eval(expr.value));
-
+// Solves the equation by sending the information to backend for processing
 $(solve).on('click', function(){
     if(validPost == true){
         var postData = {
@@ -24,23 +23,24 @@ $(solve).on('click', function(){
             "operation": expressionDropdown.value
 
         }
+        // Sends a POST request to backend though AJAX
+        // Uses the response to display the answer to the user
         $.ajax({
             type: 'POST',
             url: '/api/evaluate',
-            dataType: "json",
             contentType: 'application/json;charset=UTF-8',
             data: JSON.stringify(postData),
             success: function(responseData){
-                console.log(responseData['data']['answer'])
+
                 errorMessage = responseData['data']['errorMessage']
                 if (errorMessage == null){
                     answer = responseData['data']['answer']
-                    // result.innerHTML = math.format(node.compile().eval());
                     result.innerHTML = math.format(math.parse(answer));
 
                 } else{
                     result.innerHTML = '<span style="color: red;">' + errorMessage + '</span>';
                 }
+
             }
         });
     }
@@ -49,12 +49,9 @@ $(solve).on('click', function(){
 
 expr.oninput = function () {
     var node = null;
-
     try {
-        // parse the expression
+        // parse the expression to reprint nicely
         node = math.parse(expr.value);
-
-
         // evaluate the result of the expression
         result.innerHTML = '<span style="color: green;"> Press Solve to Evaluate!</span>';
         validPost = true;
@@ -68,14 +65,9 @@ expr.oninput = function () {
         // export the expression to LaTeX
         var latex = node ? '\\int(' + node.toTex({ parenthesis: parenthesis, implicit: implicit }) + ') dx' : '';
 
-        // console.log('LaTeX expression:', latex);
-
         // display and re-render the expression
         var elem = MathJax.Hub.getAllJax('pretty')[0];
         MathJax.Hub.Queue(['Text', elem, latex]);
 
     } catch (err){ }
 };
-// Insert AJAX POST to backend for evaluation
-// we want the backend to return an error message or the
-// evaluated expression
